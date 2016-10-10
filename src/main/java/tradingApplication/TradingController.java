76e8c1809f60225e4a865a8eca90ccfe0cbcc544
agopal94 <anirudhgopal55@gmail.com>
2016-10-10 08:36:47 +0530

    Fixed Prokect Setup
    Import this project and start since the earlier setup i pushed was not
    meant for web applications and jsp files and html werenot mapped. This
    works fine. src/main/webapp/index.jsp is the initial page which will be
    loaded

---
+++ src/main/java/tradingApplication/TradingController.java
@@ -0,0 +1,19 @@
+package tradingApplication;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class TradingController {
+
+	@RequestMapping("/test")
+	public ModelAndView welcomeMessage(
+			@RequestParam(value = "name", required = false) String name) {
+		// Name of your jsp file as parameter
+		ModelAndView view = new ModelAndView("test");
+		view.addObject("name", name);
+		return view;
+	}
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+    <h2>Hello World</h2>
+    <h3><a href="views/test.jsp">Click here...</a></h3>
+</body>
+</html>
