403e7aaf7be5a126f2971a044925b040966d6ba7
RanjiniSekar <rseka1@sapient.com>
2016-10-18 17:05:46 +0530

    saeblock

--- src/main/java/com/mock/project/main/AllocateOrderMain.java
+++ src/main/java/com/mock/project/main/AllocateOrderMain.java
@@ -8,6 +8,7 @@ import org.springframework.context.support.AbstractApplicationContext;
 import com.mock.project.config.AppConfig;
 import com.mock.project.model.Block;
 import com.mock.project.model.Order1;
+import com.mock.project.service.BrokerService;
 import com.mock.project.service.Order1Service;
 
 public class AllocateOrderMain {
@@ -20,6 +21,7 @@ public class AllocateOrderMain {
 		container.registerShutdownHook();
 		System.out.println("Inside main");
 		Order1Service order1service = (Order1Service) container.getBean("Order1Service");
+		BrokerService bs = (BrokerService) container.getBean("Broker service");
 		Order1 order = new Order1();
 		order.setSymbol("GOOGL");
 		order.setAccountType("Cash");
@@ -29,12 +31,12 @@ public class AllocateOrderMain {
 		order.setStatus("SentforExecution");
 		// order1service.save(order);
 		Block block = new Block();
-		block.setBlockId(11440L);
+		block.setBlockId(8L);
 		block.setQtyExecuted(220);
 		block.setExecutedDate(new Date("10/18/2016"));
 		block.setStatus("PartiallyAllocated");
-		order1service.updatereceivedblock(block);
-		order1service.allocateorder(block);
+		bs.saveblock(block);
+		//order1service.allocateorder(block);
 		System.out.println("Inside main1");
 
 		container.close();
