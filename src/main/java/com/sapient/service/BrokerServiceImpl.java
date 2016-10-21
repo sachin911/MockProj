265918f5a7e63392695690200a1283a0576fb70d
Aadi1995 <aadityagupta12345@gmail.com>
2016-10-21 09:33:34 +0530

    New Random

--- src/main/java/com/sapient/service/BrokerServiceImpl.java
+++ src/main/java/com/sapient/service/BrokerServiceImpl.java
@@ -24,9 +24,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
-import com.sapient.config.LoggerConfig;
 import com.sapient.dao.BlockDAO;
-import com.sapient.dao.BlockDAOImpl;
 import com.sapient.dao.SecuritiesDAO;
 import com.sapient.jms.MarshallAndSend;
 import com.sapient.dao.ViewFillsDAO;
@@ -34,11 +32,9 @@ import com.sapient.model.Block;
 import com.sapient.model.Securities;
 import com.sapient.model.ViewFills;
 
-
 @Service("brokerService")
 @Transactional(propagation = Propagation.REQUIRES_NEW)
 public class BrokerServiceImpl implements BrokerService {
-
 
 	@Autowired
 	BlockDAO blockDAO;
@@ -46,16 +42,15 @@ public class BrokerServiceImpl implements BrokerService {
 	SecuritiesDAO securitiesDAO;
 	@Autowired
 	ViewFillsDAO viewFillsDAO;
-	
+
 	private static final Logger logger = Logger.getLogger(BrokerServiceImpl.class);
 
-	
 	@Async
 	@Override
 	public void StartExecution() {
 		System.out.println("------------------------Entered Broker Service --------------------------------------");
 		List<Block> blockList = new ArrayList<>();
-		//blockList = blockDAO.findAll();
+		// blockList = blockDAO.findAll();
 		blockList = blockDAO.findOpenPartial();
 		System.out.println(blockList);
 
@@ -65,7 +60,7 @@ public class BrokerServiceImpl implements BrokerService {
 		for (Block blocks : blockList) {
 			System.out.println(blocks);
 			// delete this condition
-			String lowerCaseStatus=blocks.getStatus().toLowerCase();
+			String lowerCaseStatus = blocks.getStatus().toLowerCase();
 			if (!lowerCaseStatus.equalsIgnoreCase("Completed") && !lowerCaseStatus.equalsIgnoreCase("Expired")) {
 				// System.out.println(blocks.getStatus());
 				Securities securities = new Securities();
@@ -73,8 +68,6 @@ public class BrokerServiceImpl implements BrokerService {
 
 				// get similar block_id and set remaining quantity
 				Long remainingQty = blocks.getTotal_quantity() - blocks.getExecuted_quantity();
-
-				
 
 				securities = securitiesDAO.findByPrimaryKey(blocks.getSymbol());
 				if (securities == null) {
@@ -84,18 +77,17 @@ public class BrokerServiceImpl implements BrokerService {
 					int MaxOrderPerOrder = securities.getMax_executions();
 					double LastTradedPrice = securities.getLast_trade_price();
 					double LimitPrice = blocks.getLimit_price();
-					double StopPrice = blocks.getStop_price();
+					double stopPrice = blocks.getStop_price();
 					String side = blocks.getSide();
 					String type = blocks.getType();
-					System.out.println("type"+type);
-					/*long tempExecutedQty = 0L;
-					if (blocks.getExecuted_quantity().equals(null) || blocks.getExecuted_quantity().equals(0)) {
-						tempExecutedQty = 0;
-					} else {
-						tempExecutedQty = blocks.getExecuted_quantity();
-					}*/
-					
-
+					System.out.println("type" + type);
+					/*
+					 * long tempExecutedQty = 0L; if
+					 * (blocks.getExecuted_quantity().equals(null) ||
+					 * blocks.getExecuted_quantity().equals(0)) {
+					 * tempExecutedQty = 0; } else { tempExecutedQty =
+					 * blocks.getExecuted_quantity(); }
+					 */
 
 					long QtyExecuted = 0L;
 					long maxQtyExecuted = 0L;
@@ -104,55 +96,66 @@ public class BrokerServiceImpl implements BrokerService {
 					double priceSpred = RandomPrice(0.0, MaxPriceSpread);
 					double minPrice = LastTradedPrice - (LastTradedPrice * priceSpred / 100);
 					double maxPrice = LastTradedPrice + (LastTradedPrice * priceSpred / 100);
-					maxQtyExecuted = RandomQty(0, (long)MaxOrderPerOrder);
+					maxQtyExecuted = RandomQty(0, (long) MaxOrderPerOrder);
 					if (type.equalsIgnoreCase("market")) {
 
 						priceExecuted = RandomPrice(minPrice, maxPrice);
-						//System.out.println("priceExecuted " + priceExecuted);
+						// System.out.println("priceExecuted " + priceExecuted);
+						QtyExecuted = Math.min(maxQtyExecuted, remainingQty);
+					}
 
-						
-						QtyExecuted=Math.min(maxQtyExecuted, remainingQty);
+					else if (type.equalsIgnoreCase("limit")) {
+						if (side.equalsIgnoreCase("buy")) {
+							priceExecuted = RandomPrice(Math.min(LimitPrice, minPrice), Math.max(LimitPrice, minPrice));
+							QtyExecuted = Math.min(maxQtyExecuted, remainingQty);
+						} else {
+							priceExecuted = RandomPrice(Math.min(LimitPrice, maxPrice), Math.max(LimitPrice, maxPrice));
+							QtyExecuted = Math.min(maxQtyExecuted, remainingQty);
+						}
 					}
-					
-					else if (type.equalsIgnoreCase("limit")){
-							if( type.equalsIgnoreCase("buy")) {
-						priceExecuted = RandomPrice(Math.min(LimitPrice, minPrice), Math.max(LimitPrice, minPrice));
-						QtyExecuted=Math.min(maxQtyExecuted, remainingQty);
+					// if (type.equalsIgnoreCase("stop"))
+					else if (type.equalsIgnoreCase("stop limit")) {
+						if (stopPrice >= minPrice && stopPrice >= maxPrice) {
+							priceExecuted = stopPrice;
+							QtyExecuted = Math.min(maxQtyExecuted, remainingQty);
+						} else {
+							priceExecuted = 0;
+							QtyExecuted = 0;
+						}
 					}
-							else{
-						priceExecuted = RandomPrice(Math.min(LimitPrice, maxPrice), Math.max(LimitPrice, maxPrice));
-						QtyExecuted=Math.min(maxQtyExecuted, remainingQty);
-					}
-					}
-					//if (type.equalsIgnoreCase("stop"))
-					else  {
-						System.out.println("Type not covered in broker system");
+
+					else {
+						priceExecuted = LastTradedPrice;
+						QtyExecuted = Math.min(maxQtyExecuted, remainingQty);
+						// System.out.println("Type not covered in broker
+						// system");
 					}
 
 					// Setting the Fields
-					//System.out.println("Sock Name: "+blocks.getSymbol() );
+					// System.out.println("Sock Name: "+blocks.getSymbol() );
 
-					//System.out.println("Total Q " + blocks.getTotal_quantity());
+					// System.out.println("Total Q " +
+					// blocks.getTotal_quantity());
 					logger.info("Total Q " + blocks.getTotal_quantity());
 
-					//System.out.println("Executed Q " + QtyExecuted);
+					// System.out.println("Executed Q " + QtyExecuted);
 					logger.info("Executed Q " + QtyExecuted);
 					if (QtyExecuted != 0) {
 						blocks.setExecuted_price(priceExecuted);
-						//System.out.println("priceExecuted " + priceExecuted);
+						// System.out.println("priceExecuted " + priceExecuted);
 						blocks.setExecuted_date(new Date());
 					}
-					
+
 					blocks.setExecuted_quantity(QtyExecuted + blocks.getExecuted_quantity());
-					System.out.println("TotalQtyToExecute " + (blocks.getTotal_quantity() - blocks.getExecuted_quantity()));
-					if (blocks.getExecuted_quantity() == blocks.getTotal_quantity())
+					System.out.println(
+							"TotalQtyToExecute " + (blocks.getTotal_quantity() - blocks.getExecuted_quantity()));
+					if ((QtyExecuted + blocks.getExecuted_quantity()) == blocks.getTotal_quantity())
 						blocks.setStatus("Completed");
-					else if (QtyExecuted == 0 && !(lowerCaseStatus.equals("Partial")))
+					else if (QtyExecuted == 0 && !(lowerCaseStatus.equals("partial")))
 						blocks.setStatus("Open");
 					else
 						blocks.setStatus("Partial");
-					System.out.println("Staus: "+blocks.getStatus() );
-					
+					System.out.println("Staus: " + blocks.getStatus());
 
 					// blocks.setTotal_quantity(TotalQtyToExecute);
 					blockDAO.save(blocks);
@@ -163,7 +166,6 @@ public class BrokerServiceImpl implements BrokerService {
 						e.printStackTrace();
 					}
 
-					
 					if (QtyExecuted != 0) {
 						viewFills.setExecutedPrice(priceExecuted);
 						viewFills.setExecutedDate(new Date());
@@ -207,7 +209,6 @@ public class BrokerServiceImpl implements BrokerService {
 		return (long) (r.nextInt((int) (max - i + 1)) + i);
 	}
 
-
 	@Override
 	@Transactional(propagation = Propagation.REQUIRES_NEW)
 	public void saveblock(Block block) {
@@ -245,6 +246,6 @@ public class BrokerServiceImpl implements BrokerService {
 	@Override
 	public List<Block> openPartialView() {
 		return blockDAO.findOpenPartial();
-		 
+
 	}
 }
